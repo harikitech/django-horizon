37dd13917ef74fdd1516cf4a73d6523b9362b87a
Kosei Kitahara <surgo.jp@gmail.com>
2017-05-17 14:40:51 +0900

    Use find_package

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import find_packages, setup
+
+import horizon
+
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -10,7 +13,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'Django>=1.8',
 ]
 
 test_requirements = [
@@ -19,16 +22,16 @@ test_requirements = [
 
 setup(
     name='django-horizon',
-    version='0.0.1',
+    version=horizon.__version__,
     description="Simple database sharding (horizontal partitioning) library for Django applications.",
     long_description=readme + '\n\n' + history,
     author="UNCOVER TRUTH Inc.",
     author_email='develop@uncovertruth.co.jp',
     url='https://github.com/uncovertruth/django-horizon',
-    packages=[
-        'horizon',
-    ],
-    package_dir={'horizon': 'horizon'},
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_dir={
+        'horizon': 'horizon',
+    },
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
@@ -36,16 +39,22 @@ setup(
     keywords='django-horizon, sharding, horizontal partitioning, database, django',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Operating System :: OS Independent',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
