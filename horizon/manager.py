f20cef4a8e3838734abaae41814c9d195b651c75
Kosei Kitahara <surgo.jp@gmail.com>
2018-08-02 20:02:24 +0900

    QuerySet class as a mixin

--- horizon/manager.py
+++ horizon/manager.py
@@ -1,12 +1,11 @@
 from django.db.models.manager import Manager
 
-from .query import HorizontalQuerySet
+from .query import QuerySet
 
 
-class HorizontalManager(Manager.from_queryset(HorizontalQuerySet)):
+class HorizontalManager(Manager.from_queryset(QuerySet)):
     use_for_related_fields = True
     use_in_migrations = True
-    silence_use_for_related_fields_deprecation = True  # For Django<1.10
 
     def __init__(self):
         super(HorizontalManager, self).__init__()
--- horizon/query.py
+++ horizon/query.py
@@ -1,13 +1,13 @@
 from django.db.models import Model
-from django.db.models.query import QuerySet
+from django.db.models.query import QuerySet as DjangoQuerySet
 from django.db.utils import ProgrammingError
 
 from .utils import get_key_field_name_from_model
 
 
-class HorizontalQuerySet(QuerySet):
+class HorizontalQuerySetMixin(object):
     def __init__(self, model=None, **kwargs):
-        super(HorizontalQuerySet, self).__init__(model=model, **kwargs)
+        super(HorizontalQuerySetMixin, self).__init__(model=model, **kwargs)
         self._horizontal_key = None
 
     @classmethod
@@ -28,22 +28,22 @@ class HorizontalQuerySet(QuerySet):
 
     def _create_object_from_params(self, lookup, params):
         self._set_horizontal_key_from_params(lookup)
-        return super(HorizontalQuerySet, self)._create_object_from_params(lookup, params)
+        return super(HorizontalQuerySetMixin, self)._create_object_from_params(lookup, params)
 
     def _extract_model_params(self, defaults, **kwargs):
         self._set_horizontal_key_from_params(kwargs)
-        return super(HorizontalQuerySet, self)._extract_model_params(defaults, **kwargs)
+        return super(HorizontalQuerySetMixin, self)._extract_model_params(defaults, **kwargs)
 
     def _filter_or_exclude(self, negate, *args, **kwargs):
         self._set_horizontal_key_from_params(kwargs)
-        return super(HorizontalQuerySet, self)._filter_or_exclude(negate, *args, **kwargs)
+        return super(HorizontalQuerySetMixin, self)._filter_or_exclude(negate, *args, **kwargs)
 
     def create(self, **kwargs):
         self._set_horizontal_key_from_params(kwargs)
-        return super(HorizontalQuerySet, self).create(**kwargs)
+        return super(HorizontalQuerySetMixin, self).create(**kwargs)
 
     def _clone(self, **kwargs):
-        clone = super(HorizontalQuerySet, self)._clone(**kwargs)
+        clone = super(HorizontalQuerySetMixin, self)._clone(**kwargs)
         clone._horizontal_key = self._horizontal_key
         return clone
 
@@ -56,4 +56,8 @@ class HorizontalQuerySet(QuerySet):
             raise ProgrammingError("Missing horizontal key field's filter")
 
         self._add_hints(horizontal_key=self._horizontal_key)
-        return super(HorizontalQuerySet, self).db
+        return super(HorizontalQuerySetMixin, self).db
+
+
+class QuerySet(HorizontalQuerySetMixin, DjangoQuerySet):
+    pass
